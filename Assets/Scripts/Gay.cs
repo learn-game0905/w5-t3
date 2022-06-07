ff4bca20c188f749879902ec077e060eec6e834d
learn-game0905 <nqh149734@gmail.com>
2022-06-08 00:00:19 +0700

    Scene_02 Finish

--- Assets/Scripts/Gay.cs
+++ Assets/Scripts/Gay.cs
@@ -1,27 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gay : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private Vector3 test;
+    [SerializeField] private float speed;
+    
     private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
-
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        // Quaternion rotation = Quaternion.Euler(0f, speed, 0f);
-        // _rigidbody.MoveRotation(_rigidbody.rotation * rotation);
-        
-        // transform.Rotate(new Vector3(0f, speed, 0f));
+        //Quaternion rotation = Quaternion.Euler(0f, speed, 0f);
+        //_rigidbody.MoveRotation(_rigidbody.rotation * rotation);
+
+        //transform.Rotate(new Vector3(0f, speed, 0f));
+
+        _rigidbody.AddTorque(new Vector3(0f, 200f, 0f), ForceMode.Acceleration);
         
         // _rigidbody.rotation = Quaternion.identity;
-        
-        _rigidbody.AddTorque(test, ForceMode.Acceleration);
     }
 }
